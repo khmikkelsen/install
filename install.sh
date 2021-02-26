cc56c34f0bf5c7d756309eb75cfa8ec446e4894e
khmikkelsen <kh.mikkelsen@hotmail.com>
2021-02-26 16:02:15 +0100

    initial commit

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt -y install ./google-chrome-stable_current_amd64.deb
+
+# JetBrains Toolbox
+wget -cO jetbrains-toolbox.tar.gz "https://data.services.jetbrains.com/products/download?platform=linux&code=TBA"
+tar -xzf jetbrains-toolbox.tar.gz
+DIR=$(find . -maxdepth 1 -type d -name jetbrains-toolbox-\* -print | head -n1)
+cd $DIR
+./jetbrains-toolbox
+cd ..
+rm -r $DIR
+rm jetbrains-toolbox.tar.gz
+
+# Git
+sudo apt -y install git
+
+# Python 
+sudo apt -y install python3-pip

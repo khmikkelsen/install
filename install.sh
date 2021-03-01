97a4c9d029079eb02513c0be934e6eaaa80a12d9
khmikkelsen <kh.mikkelsen@hotmail.com>
2021-03-01 14:48:28 +0100

    Set system clock to local time

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+
+# Setup
 sudo apt update
+timedatectl set-local-rtc 1 --adjust-system-clock # make Ubuntu use local time for dual boot
+
 
 # Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
